84071b94335e2c36972ed1f9559c9b089cfdb7f6
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-10 01:19:14 +0300

    Added a constructor to the ussers class to take in data from the service api

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,13 @@
 export class Users {
+  constructor(
+    public avatat_url: string,
+    public login: string,
+    public name: string,
+    public followers: string,
+    public following: string,
+    public repos: string,
+    public url: string
+  ) {
+
+  }
 }
