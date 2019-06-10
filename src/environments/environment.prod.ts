f22d768b922cecb03ed6f27a1d9d294b1aa22ee4
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-10 08:46:36 +0300

    Added readme and license

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  secretKey: '7537a3edc5669f41e7cddbc865de2346b0c6ddca'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   secretKey: '7537a3edc5669f41e7cddbc865de2346b0c6ddca'
 };
 
